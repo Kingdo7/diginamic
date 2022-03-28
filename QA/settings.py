71dcbb6c870c1099bffdcb6bc6f2bf5108cf72ef
Louis Barillon <louis85550@live.fr>
2022-03-28 18:36:58 +0200

    [Fix] changement du chemin d'accès des fichiers html

--- QA/settings.py
+++ QA/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,8 +75,7 @@ ROOT_URLCONF = 'QA.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        'DIRS': [BASE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
