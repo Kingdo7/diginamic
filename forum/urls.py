706472d3d7b7188aa213337c849ff8da74ad6f90
Louis Barillon <louis85550@live.fr>
2022-03-31 10:08:38 +0200

    [add]  Details urls

--- forum/urls.py
+++ forum/urls.py
@@ -1,13 +1,19 @@
 from django.urls import path
 from . import views
-
+from .views import (
+    C_Question,
+    D_Question,
+    U_Question,
+    L_Question,
+)
 
 app_name = 'forum'
 
 
 urlpatterns = [
     path("create/", views.Create, name = "Question"),
-    path("<str:id>/update", views.Update, name="update"),
-    path("details/<str:id>", views.Details, name="details"),
+    #path("<slug:id>/update", views.Update, name="update"),
+    path("detail/<int:id>/", views.Details, name="details"),
     path("", views.List, name='feed'),
+
 ]
